fafbcdce26b042a89ed1087ea96e31b49c21ecab
Ben Hardill <b.hardill@gmail.com>
2013-09-27 10:55:46 +0100

    renamed pause node to delay node

---
+++ nodes/core/89-delay.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2013 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+ 
+// Simple node to introduce a pause into a flow
+
+// Require main module
+var RED = require("../../red/red");
+var idList = [];
+
+// main node definition
+function DelayNode(n) {
+   RED.nodes.createNode(this,n);
+   
+   this.timeout = n.timeout * 1000;
+   this.name = n.name
+   
+   this.on("input", function(msg) {
+       var node= this;
+       var id;
+       id = setTimeout(function(){
+              idList.splice(idList.indexOf(id),1);
+              node.send(msg);
+            }, node.timeout);
+       idList.push(id);
+   });
+}
+
+// register node
+RED.nodes.registerType("pause",DelayNode);
+
+DelayNode.prototype.close = function() {
+   for (var i=0; i<idList.length; i++ ) {
+       clearTimeout(idList[i]);
+   }
+}
