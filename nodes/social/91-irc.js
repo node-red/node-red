09348eb3534d9515db2e3366c8bb6917df8fc1a0
Dave C-J <dave@conway-jones.co.uk>
2013-11-02 12:12:47 +0000

    Improve IRC UI options to Fix Issue #53.  Now a three way select - either send whole msg object to channel, just msg.payload to channel or msg.payload to users specified in msg.topic

--- nodes/social/91-irc.js
+++ nodes/social/91-irc.js
@@ -20,72 +20,79 @@ var util = require("util");
 
 // The Server Definition - this opens (and closes) the connection
 function IRCServerNode(n) {
-	RED.nodes.createNode(this,n);
-	this.server = n.server;
-	this.channel = n.channel;
-	this.nickname = n.nickname;
-	this.ircclient = null;
-	this.on("close", function() {
-		if (this.ircclient != null) {
-			this.ircclient.disconnect();
-		}
-	});
+    RED.nodes.createNode(this,n);
+    this.server = n.server;
+    this.channel = n.channel;
+    this.nickname = n.nickname;
+    this.ircclient = null;
+    this.on("close", function() {
+        if (this.ircclient != null) {
+            this.ircclient.disconnect();
+        }
+    });
 }
 
 RED.nodes.registerType("irc-server",IRCServerNode);
 
 // The Input Node
 function IrcInNode(n) {
-	RED.nodes.createNode(this,n);
-	this.ircserver = n.ircserver;
-	this.serverConfig = RED.nodes.getNode(this.ircserver);
-	if (this.serverConfig.ircclient == null) {
-		this.serverConfig.ircclient = new irc.Client(this.serverConfig.server, this.serverConfig.nickname, {
-			channels: [this.serverConfig.channel]
-		});
-		this.serverConfig.ircclient.addListener('error', function(message) {
-			util.log('[irc] '+ JSON.stringify(message));
-		});
-	}
-	this.ircclient = this.serverConfig.ircclient;
-	var node = this;
+    RED.nodes.createNode(this,n);
+    this.ircserver = n.ircserver;
+    this.serverConfig = RED.nodes.getNode(this.ircserver);
+    if (this.serverConfig.ircclient == null) {
+        this.serverConfig.ircclient = new irc.Client(this.serverConfig.server, this.serverConfig.nickname, {
+            channels: [this.serverConfig.channel]
+        });
+        this.serverConfig.ircclient.addListener('error', function(message) {
+            util.log('[irc] '+ JSON.stringify(message));
+        });
+    }
+    this.ircclient = this.serverConfig.ircclient;
+    var node = this;
 
-	this.ircclient.addListener('message', function (from, to, message) {
-		//util.log(from + ' => ' + to + ': ' + message);
-		var msg = { "topic":from, "to":to, "payload":message };
-		node.send(msg);
-	});
+    this.ircclient.addListener('message', function (from, to, message) {
+        //util.log(from + ' => ' + to + ': ' + message);
+        var msg = { "topic":from, "to":to, "payload":message };
+        node.send(msg);
+    });
 
 }
 RED.nodes.registerType("irc in",IrcInNode);
 
 // The Output Node
 function IrcOutNode(n) {
-	RED.nodes.createNode(this,n);
-	this.sendAll = n.sendObject;
-	this.ircserver = n.ircserver;
-	this.serverConfig = RED.nodes.getNode(this.ircserver);
-	this.channel = this.serverConfig.channel;
-	if (this.serverConfig.ircclient == null) {
-		this.serverConfig.ircclient = new irc.Client(this.serverConfig.server, this.serverConfig.nickname, {
-			channels: [this.serverConfig.channel]
-		});
-		this.serverConfig.ircclient.addListener('error', function(message) {
-			util.log('[irc] '+ JSON.stringify(message));
-		});
-	}
-	this.ircclient = this.serverConfig.ircclient;
-	var node = this;
+    RED.nodes.createNode(this,n);
+    this.sendAll = n.sendObject;
+    this.ircserver = n.ircserver;
+    this.serverConfig = RED.nodes.getNode(this.ircserver);
+    this.channel = this.serverConfig.channel;
+    if (this.serverConfig.ircclient == null) {
+        this.serverConfig.ircclient = new irc.Client(this.serverConfig.server, this.serverConfig.nickname, {
+            channels: [this.serverConfig.channel]
+        });
+        this.serverConfig.ircclient.addListener('error', function(message) {
+            util.log('[irc] '+ JSON.stringify(message));
+        });
+    }
+    this.ircclient = this.serverConfig.ircclient;
+    var node = this;
 
-	this.on("input", function(msg) {
-		//console.log(msg,node.channel);
-		if (node.sendAll) {
-			node.ircclient.say(node.channel, JSON.stringify(msg));
-		}
-		else {
-			var to = msg.topic || node.channel;
-			node.ircclient.say(to, msg.payload);
-		}
-	});
+    this.on("input", function(msg) {
+        //console.log(msg,node.channel);
+        if (msg._topic) { delete msg._topic; }
+        if (node.sendAll == "false") {
+            node.ircclient.say(node.channel, JSON.stringify(msg));
+        }
+        else {
+            if (typeof msg.payload === "object") { msg.payload = JSON.stringify(msg.payload); }
+            if (node.sendAll == "pay") {
+                node.ircclient.say(node.channel, msg.payload);
+            }
+            else {
+                var to = msg.topic || node.channel;
+                node.ircclient.say(to, msg.payload);
+            }
+        }
+    });
 }
 RED.nodes.registerType("irc out",IrcOutNode);
