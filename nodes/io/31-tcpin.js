d2208fae830e157a31ce6fc7fbd00d33d96bf17c
Nicholas O'Leary <nick.oleary@gmail.com>
2013-11-14 14:39:26 +0000

    Collapse TCP nodes into single pair of files

--- nodes/io/31-tcpin.js
+++ nodes/io/31-tcpin.js
@@ -19,134 +19,235 @@ var reconnectTime = RED.settings.socketReconnectTime||10000;
 var net = require('net');
 
 function TcpIn(n) {
-	RED.nodes.createNode(this,n);
-	this.host = n.host;
-	this.port = n.port * 1;
-	this.topic = n.topic;
-	this.stream = (!n.datamode||n.datamode=='stream'); /* stream,single*/
-	this.datatype = n.datatype||'buffer'; /* buffer,utf8,base64 */
-	this.newline = (n.newline||"").replace("\\n","\n").replace("\\r","\r");
-	this.base64 = n.base64;
-	this.server = (typeof n.server == 'boolean')?n.server:(n.server == "server");
-	this.closing = false;
-	var node = this;
+    RED.nodes.createNode(this,n);
+    this.host = n.host;
+    this.port = n.port * 1;
+    this.topic = n.topic;
+    this.stream = (!n.datamode||n.datamode=='stream'); /* stream,single*/
+    this.datatype = n.datatype||'buffer'; /* buffer,utf8,base64 */
+    this.newline = (n.newline||"").replace("\\n","\n").replace("\\r","\r");
+    this.base64 = n.base64;
+    this.server = (typeof n.server == 'boolean')?n.server:(n.server == "server");
+    this.closing = false;
+    var node = this;
 
-	if (!node.server) {
+    if (!node.server) {
         var buffer = null;
-		var client;
-		var reconnectTimeout;
-		function setupTcpClient() {
-			node.log("connecting to "+node.host+":"+node.port);
-			client = net.connect(node.port, node.host, function() {
-			        buffer = (node.datatype == 'buffer')? new Buffer(0):"";
-			        node.log("connected to "+node.host+":"+node.port);
-			});
+        var client;
+        var reconnectTimeout;
+        function setupTcpClient() {
+            node.log("connecting to "+node.host+":"+node.port);
+            client = net.connect(node.port, node.host, function() {
+                    buffer = (node.datatype == 'buffer')? new Buffer(0):"";
+                    node.log("connected to "+node.host+":"+node.port);
+            });
 
-			client.on('data', function (data) {
-			        if (node.datatype != 'buffer') {
-			            data = data.toString(node.datatype);
-			        }
-			        if (node.stream) {
-			            if ((node.datatype) === "utf8" && node.newline != "") {
-			                buffer = buffer+data;
-			                var parts = buffer.split(node.newline);
-			                for (var i = 0;i<parts.length-1;i+=1) {
-			                    var msg = {topic:node.topic, payload:parts[i]};
-			                    node.send(msg);
-			                }
-			                buffer = parts[parts.length-1];
-			            } else {
-			                var msg = {topic:node.topic, payload:data};
-			                node.send(msg);
-			            }
-			        } else {
-			            if ((typeof data) === "string") {
-			                buffer = buffer+data;
-			            } else {
-			                buffer = Buffer.concat([buffer,data],buffer.length+data.length);
-			            }
-			        }
-			});
-			client.on('end', function() {
-			        if (!node.stream || (node.datatype == "utf8" && node.newline != "" && buffer.length > 0)) {
-			            var msg = {topic:node.topic,payload:buffer};
-			            node.send(msg);
-			            buffer = null;
-			        }
-			});
+            client.on('data', function (data) {
+                    if (node.datatype != 'buffer') {
+                        data = data.toString(node.datatype);
+                    }
+                    if (node.stream) {
+                        if ((node.datatype) === "utf8" && node.newline != "") {
+                            buffer = buffer+data;
+                            var parts = buffer.split(node.newline);
+                            for (var i = 0;i<parts.length-1;i+=1) {
+                                var msg = {topic:node.topic, payload:parts[i]};
+                                node.send(msg);
+                            }
+                            buffer = parts[parts.length-1];
+                        } else {
+                            var msg = {topic:node.topic, payload:data};
+                            node.send(msg);
+                        }
+                    } else {
+                        if ((typeof data) === "string") {
+                            buffer = buffer+data;
+                        } else {
+                            buffer = Buffer.concat([buffer,data],buffer.length+data.length);
+                        }
+                    }
+            });
+            client.on('end', function() {
+                    if (!node.stream || (node.datatype == "utf8" && node.newline != "" && buffer.length > 0)) {
+                        var msg = {topic:node.topic,payload:buffer};
+                        node.send(msg);
+                        buffer = null;
+                    }
+            });
 
-			client.on('close', function() {
-			        node.log("connection lost to "+node.host+":"+node.port);
-			        if (!node.closing) {
-			            reconnectTimeout = setTimeout(setupTcpClient, reconnectTime);
-			        }
-			});
+            client.on('close', function() {
+                    node.log("connection lost to "+node.host+":"+node.port);
+                    if (!node.closing) {
+                        reconnectTimeout = setTimeout(setupTcpClient, reconnectTime);
+                    }
+            });
 
-			client.on('error', function(err) {
-			        node.log(err);
-			});
-		}
-		setupTcpClient();
+            client.on('error', function(err) {
+                    node.log(err);
+            });
+        }
+        setupTcpClient();
 
-		this._close = function() {
-		    this.closing = true;
-			client.end();
-			clearTimeout(reconnectTimeout);
-		}
-	} else {
-		var server = net.createServer(function (socket) {
-		        var buffer = (node.datatype == 'buffer')? new Buffer(0):"";
-		        socket.on('data', function (data) {
-		                if (node.datatype != 'buffer') {
-		                    data = data.toString(node.datatype);
-		                }
+        this.on('close', function() {
+            this.closing = true;
+            client.end();
+            clearTimeout(reconnectTimeout);
+        });
+    } else {
+        var server = net.createServer(function (socket) {
+                var buffer = (node.datatype == 'buffer')? new Buffer(0):"";
+                socket.on('data', function (data) {
+                        if (node.datatype != 'buffer') {
+                            data = data.toString(node.datatype);
+                        }
 
-		                if (node.stream) {
-		                    if ((typeof data) === "string" && node.newline != "") {
-		                        buffer = buffer+data;
-		                        var parts = buffer.split(node.newline);
-		                        for (var i = 0;i<parts.length-1;i+=1) {
-		                            var msg = {topic:node.topic, payload:parts[i]};
-		                            node.send(msg);
-		                        }
-		                        buffer = parts[parts.length-1];
-		                    } else {
-		                        var msg = {topic:node.topic, payload:data};
-		                        node.send(msg);
-		                    }
-		                } else {
-		                    if ((typeof data) === "string") {
-		                        buffer = buffer+data;
-		                    } else {
-		                        buffer = Buffer.concat([buffer,data],buffer.length+data.length);
-		                    }
-		                }
-		        });
-		        socket.on('end', function() {
-		                if (!node.stream || (node.datatype == "utf8" && node.newline != "" && buffer.length > 0)) {
-		                    var msg = {topic:node.topic,payload:buffer};
-		                    node.send(msg);
-		                    buffer = null;
-		                }
-		        });
-		        socket.on('error',function(err) {
-		            node.log(err);
-		        });
-		});
-		server.listen(node.port);
-		node.log('listening on port '+node.port);
+                        if (node.stream) {
+                            if ((typeof data) === "string" && node.newline != "") {
+                                buffer = buffer+data;
+                                var parts = buffer.split(node.newline);
+                                for (var i = 0;i<parts.length-1;i+=1) {
+                                    var msg = {topic:node.topic, payload:parts[i],ip:socket.remoteAddress,port:socket.remotePort};
+                                    node.send(msg);
+                                }
+                                buffer = parts[parts.length-1];
+                            } else {
+                                var msg = {topic:node.topic, payload:data};
+                                node.send(msg);
+                            }
+                        } else {
+                            if ((typeof data) === "string") {
+                                buffer = buffer+data;
+                            } else {
+                                buffer = Buffer.concat([buffer,data],buffer.length+data.length);
+                            }
+                        }
+                });
+                socket.on('end', function() {
+                        if (!node.stream || (node.datatype == "utf8" && node.newline != "" && buffer.length > 0)) {
+                            var msg = {topic:node.topic,payload:buffer};
+                            node.send(msg);
+                            buffer = null;
+                        }
+                });
+                socket.on('error',function(err) {
+                    node.log(err);
+                });
+        });
+        server.listen(node.port);
+        node.log('listening on port '+node.port);
 
-		this._close = function() {
-		    this.closing = true;
-			server.close();
-			node.log('stopped listening on port '+node.port);
-		}
-	}
+        this.on('close', function() {
+            this.closing = true;
+            server.close();
+            node.log('stopped listening on port '+node.port);
+        });
+    }
 
 }
 
 RED.nodes.registerType("tcp in",TcpIn);
 
-TcpIn.prototype.close = function() {
-	this._close();
+function TcpOut(n) {
+    RED.nodes.createNode(this,n);
+    this.host = n.host;
+    this.port = n.port * 1;
+    this.base64 = n.base64;
+    this.beserver = n.beserver;
+    this.name = n.name;
+    this.closing = false;
+    var node = this;
+
+    if (!node.beserver||node.beserver=="client") {
+        var reconnectTimeout;
+        var client = null;
+        var connected = false;
+        
+        function setupTcpClient() {
+            node.log("connecting to "+node.host+":"+node.port);
+            client = net.connect(node.port, node.host, function() {
+                    connected = true;
+                    node.log("connected to "+node.host+":"+node.port);
+            });
+            
+            client.on('error', function (err) {
+                    node.log('error : '+err);
+            });
+            
+            client.on('end', function (err) {
+            });
+            
+            client.on('close', function() {
+                    node.log("connection lost to "+node.host+":"+node.port);
+                    connected = false;
+                    client.destroy();
+                    if (!node.closing) {
+                        reconnectTimeout = setTimeout(setupTcpClient,reconnectTime);
+                    }
+            });
+        }
+        setupTcpClient();
+        
+        
+        node.on("input", function(msg) {
+                if (connected && msg.payload != null) {
+                    if (Buffer.isBuffer(msg.payload)) {
+                        client.write(msg.payload);
+                    } else if (typeof msg.payload === "string" && node.base64) {
+                        client.write(new Buffer(msg.payload,'base64'));
+                    } else {
+                        client.write(new Buffer(""+msg.payload));
+                    }
+                }
+        });
+        
+        node.on("close", function() {
+            this.closing = true;
+            client.end();
+            clearTimeout(reconnectTimeout);
+        });
+        
+    } else {
+        var connectedSockets = [];
+        var server = net.createServer(function (socket) {
+                var remoteDetails = socket.remoteAddress+":"+socket.remotePort;
+                node.log("connection from "+remoteDetails);
+                connectedSockets.push(socket);
+                socket.on('close',function() {
+                        node.log("connection closed from "+remoteDetails);
+                        connectedSockets.splice(connectedSockets.indexOf(socket),1);
+                });
+                socket.on('error',function() {
+                        node.log("socket error from "+remoteDetails);
+                        connectedSockets.splice(connectedSockets.indexOf(socket),1);
+                });
+
+        });
+        node.on("input", function(msg) {
+                if (msg.payload != null) {
+                    var buffer;
+                    if (Buffer.isBuffer(msg.payload)) {
+                        buffer = msg.payload;
+                    } else if (typeof msg.payload === "string" && node.base64) {
+                        buffer = new Buffer(msg.payload,'base64');
+                    } else {
+                        buffer = new Buffer(""+msg.payload);
+                    }
+                    for (var i = 0; i<connectedSockets.length;i+=1) {
+                        connectedSockets[i].write(buffer);
+                    }
+                }
+        });
+
+        server.listen(node.port);
+        node.log('listening on port '+node.port);
+
+        node.on('close', function() {
+            server.close();
+            node.log('stopped listening on port '+node.port);
+        });
+    }
 }
+
+RED.nodes.registerType("tcp out",TcpOut);
+
+
