f051fbd1e1ca74896d06f6493b4a91644279ab00
Dave C-J <dave@conway-jones.co.uk>
2013-11-24 13:10:48 +0000

    Make imap node check for email right away on start/restart. Add some more console logging for re-assurance of things happening - or not.

--- nodes/core/social/61-imap.js
+++ nodes/core/social/61-imap.js
@@ -20,89 +20,86 @@ var util = require('util');
 var oldmail = {};
 
 try {
-	var emailkey = RED.settings.email || require(process.env.NODE_RED_HOME+"/../emailkeys.js");
-} catch(err) {
-	throw new Error("Failed to load Email credentials");
+    var emailkey = RED.settings.email || require(process.env.NODE_RED_HOME+"/../emailkeys.js");
+} catch (err) {
+    util.log("[imap] : Failed to load Email credentials");
+    return;
 }
 
 var imap = new Imap({
-	user: emailkey.user,
-	password: emailkey.pass,
-	host: emailkey.server||"imap.gmail.com",
-	port: emailkey.port||"993",
-	secure: true
+    user: emailkey.user,
+    password: emailkey.pass,
+    host: emailkey.server||"imap.gmail.com",
+    port: emailkey.port||"993",
+    secure: true
 });
 
-function fail(err) {
-	util.log('[imap] : ' + err);
-}
-
 function openInbox(cb) {
-	imap.connect(function(err) {
-		if (err) fail(err);
-		imap.openBox('INBOX', true, cb);
-	});
+    imap.connect(function(err) {
+        if (err) util.log("[imap] : error : "+err);
+        imap.openBox('INBOX', true, cb);
+    });
 }
 
 function ImapNode(n) {
-	RED.nodes.createNode(this,n);
-	this.name = n.name;
-	this.repeat = n.repeat * 1000;
-	var node = this;
-	this.interval_id = null;
+    RED.nodes.createNode(this,n);
+    this.name = n.name;
+    this.repeat = n.repeat * 1000 || 300000;
+    var node = this;
+    this.interval_id = null;
 
-	if (this.repeat && !isNaN(this.repeat) && this.repeat > 0) {
-		this.log("repeat = "+this.repeat);
-		this.interval_id = setInterval( function() {
-			node.emit("input",{});
-		}, this.repeat );
-	}
+    if (!isNaN(this.repeat) && this.repeat > 0) {
+        node.log("repeat = "+this.repeat);
+        this.interval_id = setInterval( function() {
+            node.emit("input",{});
+        }, this.repeat );
+    }
 
-	this.on("input", function(msg) {
-		openInbox(function(err, mailbox) {
-		  if (err) fail(err);
-		  imap.seq.fetch(mailbox.messages.total + ':*', { struct: false },
-			{ headers: ['from', 'subject'],
-			  body: true,
-			  cb: function(fetch) {
-				fetch.on('message', function(msg) {
-				  //node.log('Saw message no. ' + msg.seqno);
-				  var pay = {};
-				  var body = '';
-				  msg.on('headers', function(hdrs) {
-					pay.from = hdrs.from[0];
-					pay.topic = hdrs.subject[0];
-				  });
-				  msg.on('data', function(chunk) {
-					body += chunk.toString('utf8');
-				  });
-				  msg.on('end', function() {
-					pay.payload = body;
-					if ((pay.topic !== oldmail.topic)|(pay.payload !== oldmail.payload)) {
-						oldmail = pay;
-						//node.log("From: "+pay.from);
-						node.log("Subj: "+pay.topic);
-						//node.log("Body: "+pay.payload);
-						node.send(pay);
-					}
-				  });
-				});
-			  }
-			}, function(err) {
-				if (err) node.log("Err : "+err);
-				//node.log("Done fetching messages.");
-				imap.logout();
-			}
-		  );
-		});
+    this.on("input", function(msg) {
+        openInbox(function(err, mailbox) {
+          if (err) node.log("error : "+err);
+          imap.seq.fetch(mailbox.messages.total + ':*', { struct: false },
+            { headers: ['from', 'subject'],
+              body: true,
+              cb: function(fetch) {
+                fetch.on('message', function(msg) {
+                  node.log('Read message no. ' + msg.seqno);
+                  var pay = {};
+                  var body = '';
+                  msg.on('headers', function(hdrs) {
+                    pay.from = hdrs.from[0];
+                    pay.topic = hdrs.subject[0];
+                  });
+                  msg.on('data', function(chunk) {
+                    body += chunk.toString('utf8');
+                  });
+                  msg.on('end', function() {
+                    pay.payload = body;
+                    if ((pay.topic !== oldmail.topic)|(pay.payload !== oldmail.payload)) {
+                        oldmail = pay;
+                        //node.log("From: "+pay.from);
+                        node.log("Subj: "+pay.topic);
+                        //node.log("Body: "+pay.payload);
+                        node.send(pay);
+                    }
+                  });
+                });
+              }
+            }, function(err) {
+                if (err) node.log("error : "+err);
+                node.log("Done fetching messages.");
+                imap.logout();
+            }
+          );
+        });
+    });
 
-	});
+    this.on("close", function() {
+        if (this.interval_id != null) {
+            clearInterval(this.interval_id);
+        }
+    });
 
-	this.on("close", function() {
- 		if (this.interval_id != null) {
-        	clearInterval(this.interval_id);
-    	}
-	});
+    node.emit("input",{});
 }
-
 RED.nodes.registerType("imap",ImapNode);
