9a91298c6f180247502eb7170457b50d52255000
tjkoury <tj@tjkoury.com>
2014-04-29 08:50:51 -0400

    reverted Gruntfile.js, using Gulp for dev for the time being

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,24 @@
-var child_process = require('child_process');
-
 module.exports = function(grunt) {
     
     // Project configuration.
     grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      simplemocha: {
-          options: {
-              globals: ['expect'],
-              timeout: 3000,
-              ignoreLeaks: false,
-              ui: 'bdd',
-              reporter: 'tap'
-          },
-          all: { src: ['test/*.js'] }
-      },
-      watch: {
-        server: {
-          files: [
-            "./**/*.js"
-            ]
-        }
-      }
+            pkg: grunt.file.readJSON('package.json'),
+            simplemocha: {
+                options: {
+                    globals: ['expect'],
+                    timeout: 3000,
+                    ignoreLeaks: false,
+                    ui: 'bdd',
+                    reporter: 'tap'
+                },
+                all: { src: ['test/*.js'] }
+            }
     });
     
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-simple-mocha');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     
     // Default task(s).
-    grunt.registerTask('test', ['simplemocha']);
-    grunt.registerTask('default', function(){
-     
-      var child_express_app = child_process.fork('red');
-      
-      grunt.event.on('watch', function(action, filepath, target) {
-        grunt.log.writeln('Restarting node-blue webapp');
-        child_express_app.kill();
-        child_express_app = child_process.fork('red');
-      })
-    });
+    grunt.registerTask('default', ['simplemocha']);
+    
 };
