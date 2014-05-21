26d6547a12478f8dd7c964e218e751cefa28d846
tjkoury <tj@tjkoury.com>
2014-05-21 11:00:17 -0400

    fixed string detection issue causing typeerror if payload is unmodified

--- nodes/core/io/21-httpin.js
+++ nodes/core/io/21-httpin.js
@@ -120,7 +120,9 @@ module.exports = function(RED) {
                     msg.res.set(msg.headers);
                 }
                 var statusCode = msg.statusCode || 200;
-				msg.res.setHeader('content-length', Buffer.byteLength(msg.payload));
+				if(typeof msg.payload == "string"){
+					msg.res.setHeader('content-length', Buffer.byteLength(msg.payload));
+				}
                 if (typeof msg.payload == "object" && !Buffer.isBuffer(msg.payload)) {
                     msg.res.jsonp(statusCode,msg.payload);
                 } else {
