7226f8cdb46745fcbc7c8f852ad2b1ec17fdf4b5
Ben Hardill <b.hardill@gmail.com>
2014-06-01 08:43:44 +0100

    added copywrite header and some a error message if install/uninstall not passed on the cmd line

--- install-service.js
+++ install-service.js
@@ -1,3 +1,19 @@
+/**
+* Copyright 2013 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
+
 if (process.platform === 'win32') {
   var Service = require('node-windows').Service;
 } else if (process.platform === 'darwin') {
@@ -30,4 +46,6 @@ if (process.argv.length == 3) {
   } else if ( process.argv[2] == 'uninstall' ) {
     svc.uninstall();
   }
+} else {
+  concole.log('Run with the command line argument of either install or uninstall');
 }
