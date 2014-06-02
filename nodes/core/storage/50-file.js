2c9f072888d9f712a2472dcf64981873528a3868
tjkoury <tj@tjkoury.com>
2014-06-02 19:59:19 -0400

    added directory functionality to file node

--- nodes/core/storage/50-file.js
+++ nodes/core/storage/50-file.js
@@ -15,18 +15,19 @@
  **/
 
 module.exports = function(RED) {
+    "use strict";
     var fs = require("fs");
-    
+
     function FileNode(n) {
         RED.nodes.createNode(this,n);
-    
+
         this.filename = n.filename;
         this.appendNewline = n.appendNewline;
         this.overwriteFile = n.overwriteFile;
         var node = this;
         this.on("input",function(msg) {
             var filename = msg.filename || this.filename;
-    
+
             if (filename == "") {
                 node.warn('No filename specified');
             } else if (typeof msg.payload != "undefined") {
@@ -60,7 +61,7 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("file",FileNode);
-    
+
     function FileInNode(n) {
         RED.nodes.createNode(this,n);
     
