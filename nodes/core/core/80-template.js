de352dcdc23730f0be45c108b8ba4bdb1e70a2c4
Dave C-J <dave@conway-jones.co.uk>
2014-07-10 08:23:44 +0100

    Change template node to allow templating of any property of msg

--- nodes/core/core/80-template.js
+++ nodes/core/core/80-template.js
@@ -17,20 +17,43 @@
 module.exports = function(RED) {
     "use strict";
     var mustache = require("mustache");
-    var util = require("util");
     var fs = require('fs');
 
     function TemplateNode(n) {
         RED.nodes.createNode(this,n);
         this.name = n.name;
+        this.field = n.field || "payload";
         this.template = n.template;
-        this.on("input", function(msg) {
+        var node = this;
+
+        var b = node.field.split(".");
+        var i = 0;
+        var m = null;
+        var rec = function(obj) {
+            i += 1;
+            if ((i < b.length) && (typeof obj[b[i-1]] === "object")) {
+                rec(obj[b[i-1]]); // not there yet - carry on digging
+            }
+            else {
+                 if (i === b.length) { // we've finished so assign the value
+                     obj[b[i-1]] = mustache.render(node.template,m);
+                     node.send(m);
+                 }
+                 else {
+                     obj[b[i-1]] = {}; // needs to be a new object so create it
+                     rec(obj[b[i-1]]); // and carry on digging
+                 }
+            }
+        }
+
+        node.on("input", function(msg) {
             if (msg != null) {
                 try {
-                    msg.payload = mustache.render(this.template,msg)
-                    this.send(msg);
+                    m = msg;
+                    i = 0;
+                    rec(msg);
                 } catch(err) {
-                    this.error(err.message);
+                    node.error(err.message);
                 }
             }
         });
