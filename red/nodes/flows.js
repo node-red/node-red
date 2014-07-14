fe9ff0a297bbb0abf4640bd77b035a5182a12bff
Mark Hindess <mark.hindess@gmail.com>
2014-07-14 21:46:36 +0100

    Fix jshint complaints.

--- red/nodes/flows.js
+++ red/nodes/flows.js
@@ -80,7 +80,7 @@ var parseConfig = function() {
                 }
             }
             // console.log(nn);
-            if (nn == null) {
+            if (nn === null) {
                 util.log("[red] unknown type: "+activeConfig[i].type);
             }
         }
@@ -88,7 +88,7 @@ var parseConfig = function() {
     // Clean up any orphaned credentials
     credentials.clean(flowNodes.get);
     events.emit("nodes-started");
-}
+};
 
 
 function stopFlows() {
@@ -161,7 +161,7 @@ var flowNodes = module.exports = {
                 activeConfig = conf;
                 parseConfig();
             });
-        })
+        });
     },
     stopFlows: stopFlows
-}
+};
