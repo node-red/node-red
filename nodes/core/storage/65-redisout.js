d9921effd3f2ff996b34be5b76a86bbfbf4cc4d6
Mark Hindess <mark.hindess@gmail.com>
2014-07-17 22:50:13 +0100

    Modify to use redis-server config node.

--- nodes/core/storage/65-redisout.js
+++ nodes/core/storage/65-redisout.js
@@ -22,29 +22,30 @@ module.exports = function(RED) {
 
     function RedisOutNode(n) {
         RED.nodes.createNode(this,n);
-        this.port = n.port||"6379";
-        this.hostname = n.hostname||"127.0.0.1";
         this.key = n.key;
         this.structtype = n.structtype;
+        this.server = n.server;
+        this.serverConfig = RED.nodes.getNode(this.server);
+        if (this.serverConfig) {
+            this.client = redisConnectionPool.get(this.serverConfig.host,
+                                                  this.serverConfig.port);
 
-        this.client = redisConnectionPool.get(this.hostname,this.port);
+            if (this.client.connected) {
+                this.status({fill:"green",shape:"dot",text:"connected"});
+            } else {
+                this.status({fill:"red",shape:"ring",text:"disconnected"},true);
+            }
 
-        if (this.client.connected) {
-            this.status({fill:"green",shape:"dot",text:"connected"});
-        } else {
-            this.status({fill:"red",shape:"ring",text:"disconnected"},true);
-        }
+            var node = this;
+            this.client.on("end", function() {
+                node.status({fill:"red",shape:"ring",text:"disconnected"});
+            });
+            this.client.on("connect", function() {
+                node.status({fill:"green",shape:"dot",text:"connected"});
+            });
 
-        var node = this;
-        this.client.on("end", function() {
-            node.status({fill:"red",shape:"ring",text:"disconnected"});
-        });
-        this.client.on("connect", function() {
-            node.status({fill:"green",shape:"dot",text:"connected"});
-        });
-
-        this.on("input", function(msg) {
-                if (msg != null) {
+            this.on("input", function(msg) {
+                if (msg !== null) {
                     var k = this.key || msg.topic;
                     if (k) {
                         if (this.structtype == "string") {
@@ -65,10 +66,20 @@ module.exports = function(RED) {
                         this.warn("No key or topic set");
                     }
                 }
-        });
-        this.on("close", function() {
-            redisConnectionPool.close(node.client);
-        });
+            });
+            this.on("close", function() {
+                redisConnectionPool.close(node.client);
+            });
+        } else {
+            this.error("missing redis server configuration");
+        }
     }
     RED.nodes.registerType("redis out",RedisOutNode);
-}
+
+    function RedisServerNode(n) {
+        RED.nodes.createNode(this,n);
+        this.host = n.host;
+        this.port = n.port;
+    }
+    RED.nodes.registerType("redis-server", RedisServerNode);
+};
