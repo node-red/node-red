04e4f716b623c7756331061c3c121f1a126c131b
hbeeken <hbeeken@users.noreply.github.com>
2014-07-21 13:30:50 +0100

    Adding test for node/index.js

--- test/red/nodes/index_spec.js
+++ test/red/nodes/index_spec.js
@@ -14,9 +14,69 @@
  * limitations under the License.
  **/
 var should = require("should");
+var should = require("should");
+var fs = require('fs-extra');
+var path = require('path');
+var when = require("when");
+var defer = when.defer();
+var RedNode = require("../../../red/nodes/Node");
+var index = require("../../../red/nodes/index");
 
 describe("red/nodes/index", function() {
     it('can be required without errors', function() {
         require("../../../red/nodes/index");
     });
+    
+   it('should initialise crendentials, flows and registry',function(done) {
+       var testFlows = [{"type":"tab","id":"tab1","label":"Sheet 1"}];
+        var storage = {
+                getFlows: function() {
+                    var defer = when.defer();
+                    defer.resolve(testFlows);
+                    return defer.promise;
+                },
+                getCredentials: function() {
+                    console.log("ONE");
+                    return when.promise(function(resolve,reject) {
+                        resolve({"tab1":{"b":1,"c":2}});
+                    });
+                },
+                saveFlows: function(conf) {
+                    var defer = when.defer();
+                    defer.resolve();
+                    should.deepEqual(testFlows, conf);
+                    return defer.promise;
+                },
+                saveCredentials: function(creds) {
+                    return when(true);
+                }
+         };
+        
+        function TestNode(n) {
+            this.id = 'tab1';
+            this.type = 'abc';
+            this.name = 'barney';
+            var node = this;
+
+            this.on("log", function() {
+                // do nothing
+            });
+        }
+        
+        index.init({}, storage);
+        index.registerType('test', TestNode);
+        var testnode = new TestNode({id:'tab1',type:'abc',name:'barney'});
+        index.createNode(testnode,{id:'tab1',type:'abc',name:'barney'});
+        index.loadFlows().then(function() {
+                should.deepEqual(testFlows, index.getFlows());
+                var creds = index.getCredentials("tab1");
+                creds.should.have.property('b',1);
+                creds.should.have.property('c',2);
+                done();
+        }).otherwise(function(err) {
+            done(err);
+        });
+
+    });
+   
 });
