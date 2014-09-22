1f95071a37b8121906cdd128116530eb3c8e9b7e
Nick O'Leary <nick.oleary@gmail.com>
2014-09-22 20:44:22 +0100

    Only normalise known headers in http node

--- nodes/core/io/21-httpin.js
+++ nodes/core/io/21-httpin.js
@@ -171,7 +171,13 @@ module.exports = function(RED) {
             if (msg.headers) {
                 for (var v in msg.headers) {
                     if (msg.headers.hasOwnProperty(v)) {
-                        opts.headers[v.toLowerCase()] = msg.headers[v];
+                        var name = v.toLowerCase();
+                        if (name !== "content-type" && name !== "content-length") {
+                            // only normalise the known headers used later in this
+                            // function. Otherwise leave them alone.
+                            name = v;
+                        }
+                        opts.headers[name] = msg.headers[v];
                     }
                 }
             }
