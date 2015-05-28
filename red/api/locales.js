d9df92f79c713b3a48bee5812961b9bd39a08dc5
Allen Boone <kallenboone@gmail.com>
2015-05-28 15:17:31 -0400

    locale exposed to editor

--- red/api/locales.js
+++ red/api/locales.js
@@ -19,8 +19,12 @@ module.exports = {
     get: function(req,res) {
         var namespace = req.params[0];
         namespace = namespace.replace(/\.json$/,"");
-        var lang = req.query["lang"]||"en-US"; // TODO: determine requested lang
-        var catalog = i18n.catalog(namespace,lang);
-        res.json(catalog||{});
+        var lang = i18n.determineLangFromHeaders(req.acceptedLanguages);
+        var prevLang = i18n.i.lng();
+        i18n.i.setLng(lang, function(){
+            var catalog = i18n.catalog(namespace,lang);
+            res.json(catalog||{});
+        });
+        i18n.i.setLng(prevLang);
     }
 }
