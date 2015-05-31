974ac31d33a3e6dc5012348e3b8eb2a2d6b3a73a
dceejay <conway@uk.ibm.com>
2015-05-31 12:58:42 +0100

    Hide right sidebar on start if screen narrower than 600px
    so at last you can see something that looks like Node-RED

--- editor/js/ui/sidebar.js
+++ editor/js/ui/sidebar.js
@@ -141,6 +141,8 @@ RED.sidebar = (function() {
         RED.keyboard.add(/* SPACE */ 32,{ctrl:true},function(){RED.menu.setSelected("menu-item-sidebar",!RED.menu.isSelected("menu-item-sidebar"));d3.event.preventDefault();});
         showSidebar();
         RED.sidebar.info.show();
+        // hide info bar at start if screen rather narrow...
+        if ($(window).width() < 600) { toggleSidebar(); }
     }
     
     var eventHandler = (function() {
