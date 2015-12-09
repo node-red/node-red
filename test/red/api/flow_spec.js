ca62e720b5ddd8bba5112196ac16475cec25eea9
Nick O'Leary <nick.oleary@gmail.com>
2015-12-09 21:59:42 +0000

    Add missing spec file

---
+++ test/red/api/flow_spec.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var should = require("should");
+var request = require('supertest');
+var express = require('express');
+var bodyParser = require('body-parser');
+var sinon = require('sinon');
+var when = require('when');
+
+var flows = require("../../../red/api/flows");
+
+describe("flow api", function() {
+
+    var app;
+
+    it.skip('add flow');
+    it.skip('update flow');
+    it.skip('remove flow');
+    it.skip('get flow');
+
+
+});
