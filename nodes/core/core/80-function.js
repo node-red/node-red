742c470d81f3b1aab71b1df3b4a752d8875ee75d
Nick O'Leary <nick.oleary@gmail.com>
2015-12-29 22:17:30 +0000

    Add context/flow/global support to Function node

--- nodes/core/core/80-function.js
+++ nodes/core/core/80-function.js
@@ -90,7 +90,34 @@ module.exports = function(RED) {
                 }
             },
             context: {
-                global:RED.settings.functionGlobalContext || {}
+                set: function() {
+                    node.context().set.apply(node,arguments);
+                },
+                get: function() {
+                    return node.context().get.apply(node,arguments);
+                },
+                get global() {
+                    return node.context().global;
+                },
+                get flow() {
+                    return node.context().flow;
+                }
+            },
+            flow: {
+                set: function() {
+                    node.context().flow.set.apply(node,arguments);
+                },
+                get: function() {
+                    return node.context().flow.get.apply(node,arguments);
+                }
+            },
+            global: {
+                set: function() {
+                    node.context().global.set.apply(node,arguments);
+                },
+                get: function() {
+                    return node.context().global.get.apply(node,arguments);
+                }
             },
             setTimeout: function () {
                 var func = arguments[0];
