859a7538e1a95fd122220643400294b180319a6f
Nick O'Leary <nick.oleary@gmail.com>
2016-02-26 10:35:15 +0000

    Fix crash on repeated inject of invalid json payload

--- nodes/core/core/20-inject.js
+++ nodes/core/core/20-inject.js
@@ -50,18 +50,22 @@ module.exports = function(RED) {
         }
 
         this.on("input",function(msg) {
-            var msg = {topic:this.topic};
-            if ( (this.payloadType == null && this.payload === "") || this.payloadType === "date") {
-                msg.payload = Date.now();
-            } else if (this.payloadType == null) {
-                msg.payload = this.payload;
-            } else if (this.payloadType == 'none') {
-                msg.payload = "";
-            } else {
-                msg.payload = RED.util.evaluateNodeProperty(this.payload,this.payloadType,this,msg);
+            try {
+                msg = {topic:this.topic};
+                if ( (this.payloadType == null && this.payload === "") || this.payloadType === "date") {
+                    msg.payload = Date.now();
+                } else if (this.payloadType == null) {
+                    msg.payload = this.payload;
+                } else if (this.payloadType == 'none') {
+                    msg.payload = "";
+                } else {
+                    msg.payload = RED.util.evaluateNodeProperty(this.payload,this.payloadType,this,msg);
+                }
+                this.send(msg);
+                msg = null;
+            } catch(err) {
+                this.error(err,msg);
             }
-            this.send(msg);
-            msg = null;
         });
     }
 
