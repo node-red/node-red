63dc1a4f629e29bf0172bee1dc4127c2739745c4
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-03-13 16:26:23 +0100

    get port from environment by default

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@
 
 module.exports = {
     // the tcp port that the Node-RED web server is listening on
-    uiPort: 1880,
+    uiPort: process.env.PORT || 1880,
 
     // By default, the Node-RED UI accepts connections on all IPv4 interfaces.
     // The following property can be used to listen on a specific interface. For
