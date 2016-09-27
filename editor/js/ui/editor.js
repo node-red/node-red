83a69b39ae02542436c4cba351b1eaff12b147c7
Moshe Wajnberg <wajnberg@il.ibm.com>
2016-09-27 14:21:43 +0300

    check input.val()

--- editor/js/ui/editor.js
+++ editor/js/ui/editor.js
@@ -316,7 +316,7 @@ RED.editor = (function() {
                 RED.text.format.attach(input[0], definition[property].format, {}, false, "en");
             } else {
                 input.val(val);
-                if (val.length !== undefined) {
+                if (input.val() !== undefined) {
                     RED.text.bidi.prepareInput(input);
                 }
             }
