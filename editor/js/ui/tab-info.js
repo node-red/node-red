185e2ca19993b964d423f011efe2de84fcb1f6e8
samarsultan <samarsultan.1993@gmail.com>
2016-10-18 13:46:03 +0200

    adding bidi support

--- editor/js/ui/tab-info.js
+++ editor/js/ui/tab-info.js
@@ -70,7 +70,7 @@ RED.sidebar.info = (function() {
         var table = '<table class="node-info"><tbody>';
         table += '<tr class="blank"><td colspan="2">'+RED._("sidebar.info.node")+'</td></tr>';
         if (node.type != "subflow" && node.name) {
-            table += '<tr><td>'+RED._("common.label.name")+'</td><td>&nbsp;<span class="bidiAware" dir="'+RED.text.bidi.resolveBaseTextDir(node.name)+'">'+node.name+'</span></td></tr>';
+            table += '<tr><td>'+RED._("common.label.name")+'</td><td>&nbsp;<span class="bidiAware" dir="'+RED.bidiUtil.resolveBaseTextDir(node.name)+'">'+RED.bidiUtil.applyBidiSupport(node.name,BidiFlags.NS)+'</span></td></tr>';
         }
         table += "<tr><td>"+RED._("sidebar.info.type")+"</td><td>&nbsp;"+node.type+"</td></tr>";
         table += "<tr><td>"+RED._("sidebar.info.id")+"</td><td>&nbsp;"+node.id+"</td></tr>";
@@ -93,8 +93,8 @@ RED.sidebar.info = (function() {
                     userCount++;
                 }
             });
-            table += '<tr><td>'+RED._("common.label.name")+'</td><td><span class="bidiAware" dir=\"'+RED.text.bidi.resolveBaseTextDir(subflowNode.name)+'">'+subflowNode.name+'</span></td></tr>';
-            table += "<tr><td>"+RED._("sidebar.info.instances")+"</td><td>"+userCount+"</td></tr>";
+            table += '<tr><td>'+RED._("common.label.name")+'</td><td><span class="bidiAware" dir=\"'+RED.bidiUtil.resolveBaseTextDir(subflowNode.name)+'">'+RED.bidiUtil.applyBidiSupport(subflowNode.name,BidiFlags.NS)+'</span></td></tr>';
+            table += "<tr><td>"+RED._("sidebar.info.instances")+"</td><td>"+RED.bidiUtil.applyBidiSupport(userCount,BidiFlags.NS)+"</td></tr>";
         }
 
         if (!m && node.type != "subflow" && node.type != "comment") {
@@ -132,7 +132,7 @@ RED.sidebar.info = (function() {
                             val = val.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
                         }
 
-                        table += '<tr class="node-info-property-row'+(propertiesExpanded?"":" hide")+'"><td>'+n+"</td><td>"+val+"</td></tr>";
+                        table += '<tr class="node-info-property-row'+(propertiesExpanded?"":" hide")+'"><td>'+n+"</td><td>"+RED.bidiUtil.applyBidiSupport(val,BidiFlags.NS)+"</td></tr>";
                     }
                 }
             }
@@ -140,14 +140,14 @@ RED.sidebar.info = (function() {
         table += "</tbody></table><hr/>";
         if (!subflowNode && node.type != "comment") {
             var helpText = $("script[data-help-name$='"+node.type+"']").html()||"";
-            table  += '<div class="node-help"><span class="bidiAware" dir=\"'+RED.text.bidi.resolveBaseTextDir(helpText)+'">'+helpText+'</span></div>';
+            table  += '<div class="node-help"><span class="bidiAware" dir=\"'+RED.bidiUtil.resolveBaseTextDir(helpText)+'">'+RED.bidiUtil.applyBidiSupport(helpText,BidiFlags.NS)+'</span></div>';
         }
         if (subflowNode) {
-            table += '<div class="node-help"><span class="bidiAware" dir=\"'+RED.text.bidi.resolveBaseTextDir(subflowNode.info||"")+'">'+marked(subflowNode.info||"")+'</span></div>';
+            table += '<div class="node-help"><span class="bidiAware" dir=\"'+RED.bidiUtil.resolveBaseTextDir(subflowNode.info||"")+'">'+marked(subflowNode.info||"")+'</span></div>';
         } else if (node._def && node._def.info) {
             var info = node._def.info;
             var textInfo = (typeof info === "function" ? info.call(node) : info);
-            table += '<div class="node-help"><span class="bidiAware" dir=\"'+RED.text.bidi.resolveBaseTextDir(textInfo)+'">'+marked(textInfo)+'</span></div>';
+            table += '<div class="node-help"><span class="bidiAware" dir=\"'+RED.bidiUtil.resolveBaseTextDir(textInfo)+'">'+marked(textInfo)+'</span></div>';
             //table += '<div class="node-help">'+(typeof info === "function" ? info.call(node) : info)+'</div>';
         }
 
