95f41c293ee42af9a1abea4c7d39a88c381e957f
Ashamandi <ashamand@eg.ibm.com>
2016-11-22 09:46:39 +0200

    Fixing Popover mirroring defect

--- editor/js/ui/common/popover.js
+++ editor/js/ui/common/popover.js
@@ -34,8 +34,8 @@ RED.popover = (function() {
                 var targetHeight = target.height();
 
                 var divHeight = div.height();
-                div.css({top: targetPos.top+targetHeight/2-divHeight/2-10,left:targetPos.left+targetWidth+17});
-
+                var popoverPos = ((RED.bidiUtil.isMirroringEnabled()) ? targetPos.left-targetWidth-210 : targetPos.left+targetWidth+17);
+                div.css({top: targetPos.top+targetHeight/2-divHeight/2-10,left:popoverPos});
                 div.fadeIn("fast");
             }
         }
