2096b660f3317ff0b2cd415ea4f6f32884043afd
Josh Shaw <josh@tothereandback.co.nz>
2016-12-09 11:28:08 +1300

    update welcome message to use logger so it can be turned off/on if required

--- red/runtime/index.js
+++ red/runtime/index.js
@@ -92,7 +92,7 @@ function start() {
                     reportMetrics();
                 }, settings.runtimeMetricInterval||15000);
             }
-            console.log("\n\n"+log._("runtime.welcome")+"\n===================\n");
+            log.info("\n\n"+log._("runtime.welcome")+"\n===================\n");
             if (settings.version) {
                 log.info(log._("runtime.version",{component:"Node-RED",version:"v"+settings.version}));
             }
