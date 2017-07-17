dc6e4013bfc6d1b35e61fbb96eeca8dd4e53fe4e
MatthewColegate <mcolegate@aol.com>
2017-07-17 11:48:44 +0100

    enable appmetrics-dash if APPMETRICS env variable set

--- red.js
+++ red.js
@@ -14,6 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
+var dash;
+if (process.env.APPMETRICS) {
+    dash = require('appmetrics-dash');
+    dash.attach();
+}
 var http = require('http');
 var https = require('https');
 var util = require("util");
