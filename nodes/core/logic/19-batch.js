5b74e116d4198dd6998d512a764ecc8625dad5e8
Hiroyasu Nishiyama <hiroyasu.nishiyama.uq@hitachi.com>
2017-12-18 12:55:42 +0900

    add concat mode & fix for docs and js code

--- nodes/core/logic/19-batch.js
+++ nodes/core/logic/19-batch.js
@@ -17,65 +17,199 @@
 module.exports = function(RED) {
     "use strict";
 
-    function send_msgs(node, msgs) {
+    var _max_kept_msgs_count = undefined;
+
+    function max_kept_msgs_count(node) {
+        if (_max_kept_msgs_count === undefined) {
+            var name = "batchMaxKeptMsgsCount";
+            if (RED.settings.hasOwnProperty(name)) {
+                _max_kept_msgs_count = RED.settings[name];
+            }
+            else {
+                _max_kept_msgs_count = 0;
+            }
+        }
+        return _max_kept_msgs_count;
+    }
+
+    function send_msgs(node, msgs, clone_msg) {
         var count = msgs.length;
         var msg_id = msgs[0]._msgid;
-        for(var i = 0; i < count; i++) {
-            var msg = RED.util.cloneMessage(msgs[i]);
-            msg.parts = {
-                id: msg_id,
-                index: i,
-                count:count
-            };
+        for (var i = 0; i < count; i++) {
+            var msg = clone_msg ? RED.util.cloneMessage(msgs[i]) : msgs[i];
+            if (!msg.hasOwnProperty("parts")) {
+                msg.parts = {};
+            }
+            var parts = msg.parts;
+            parts.id = msg_id;
+            parts.index = i;
+            parts.count = count;
             node.send(msg);
         }
     }
     
+    function send_interval(node, allow_empty_seq) {
+        let msgs = node.msg_queue;
+        if (msgs.length > 0) {
+            send_msgs(node, msgs, false);
+            node.msg_queue = [];
+        }
+        else {
+            if (allow_empty_seq) {
+                let mid = RED.util.generateId();
+                let msg = {
+                    payload: null,
+                    parts: {
+                        id: mid,
+                        index: 0,
+                        count: 0
+                    }
+                };
+                node.send(msg);
+            }
+        }
+    }
+
+    function is_complete(pending, topic) {
+        if (pending.hasOwnProperty(topic)) {
+            var p_topic = pending[topic];
+            var gids = p_topic.gids;
+            if (gids.length > 0) {
+                var gid = gids[0];
+                var groups = p_topic.groups;
+                var group = groups[gid];
+                return (group.count === group.msgs.length);
+            }
+        }
+        return false;
+    }
+
+    function get_msgs_of_topic(pending, topic) {
+        var p_topic = pending[topic];
+        var groups = p_topic.groups;
+        var gids = p_topic.gids;
+        var gid = gids[0];
+        var group = groups[gid];
+        return group.msgs;
+    }
+
+    function remove_topic(pending, topic) {
+        var p_topic = pending[topic];
+        var groups = p_topic.groups;
+        var gids = p_topic.gids;
+        var gid = gids.shift();
+        delete groups[gid];
+    }
+
+    function try_concat(node, pending) {
+        var topics = node.topics;
+        for (var topic of topics) {
+            if (!is_complete(pending, topic)) {
+                return;
+            }
+        }
+        var msgs = [];
+        for (var topic of topics) {
+            var t_msgs = get_msgs_of_topic(pending, topic);
+            msgs = msgs.concat(t_msgs);
+        }
+        for (var topic of topics) {
+            remove_topic(pending, topic);
+        }
+        send_msgs(node, msgs, false);
+        node.pending_count -= msgs.length;
+    }
+    
+    function add_to_topic_group(pending, topic, gid, msg) {
+        if (!pending.hasOwnProperty(topic)) {
+            pending[topic] = { groups: {}, gids: [] };
+        }
+        var p_topic = pending[topic];
+        var groups = p_topic.groups;
+        var gids = p_topic.gids;
+        if (!groups.hasOwnProperty(gid)) {
+            groups[gid] = { msgs: [], count: undefined };
+            gids.push(gid);
+        }
+        var group = groups[gid];
+        group.msgs.push(msg);
+        if ((group.count === undefined) &&
+            msg.parts.hasOwnProperty('count')) {
+            group.count = msg.parts.count;
+        }
+    }
+
+    function concat_msg(node, msg) {
+        var topic = msg.topic;
+        if(node.topics.indexOf(topic) >= 0) {
+            var gid = msg.parts.id;
+            var pending = node.pending;
+            add_to_topic_group(pending, topic, gid, msg);
+            node.pending_count++;
+            var max_msgs = max_kept_msgs_count(node);
+            if ((max_msgs > 0) && (node.pending_count > max_msgs)) {
+                node.pending = {};
+                node.pending_count = 0;
+                throw new Error("too many pending messages");
+            }
+            try_concat(node, pending);
+        }
+    }
+
     function BatchNode(n) {
         RED.nodes.createNode(this,n);
         var node = this;
-        var context = node.context();
         var mode = n.mode || "count";
 
-        if(mode === "count") {
+        if (mode === "count") {
             var count = Number(n.count || 1);
             var overwrap = Number(n.overwrap || 0);
-            var interval = count -overwrap;
-            var q_next = 0;
+            var is_overwrap = (overwrap > 0);
+            if (count <= overwrap) {
+                this.error("Invalid count and overwrap");
+                return;
+            }
             var queue = [];
             this.on("input", function(msg) {
-                if(q_next == 0) {
-                    queue.push([]);
-                    q_next = interval;
+                queue.push(msg);
+                var len = queue.length;
+                if (len === count) {
+                    send_msgs(queue, msg, is_overwrap);
+                    queue = (overwrap === 0) ? [] :  queue.slice(-overwrap);
                 }
-                var new_queue = [];
-                let len = queue.length;
-                for(var i = 0; i < len; i++) {
-                    var msgs = queue.shift();
-                    msgs.push(msg);
-                    if(msgs.length == count) {
-                        send_msgs(node, msgs);
-                    }
-                    else {
-                        new_queue.push(msgs);
-                    }
-                }
-                q_next--;
-                queue = new_queue;
+            });
+            this.on("close", function() {
+                queue = [];
             });
         }
-        else if(mode === "interval") {
-            var msgs = [];
-            var interval = Number(n.interval || 1) *1000;
+        else if (mode === "interval") {
+            var interval = parseInt(n.interval || "0") *1000;
+            var allow_empty_seq = n.allowEmptySequence;
+            node.msg_queue = []
+            var timer = setInterval(function() {
+                send_interval(node, allow_empty_seq);
+            }, node.batch_interval);
             this.on("input", function(msg) {
-                msgs.push(msg);
+                node.msg_queue.push(msg);
             });
-            setInterval(function() {
-                if(msgs.length > 0) {
-                    send_msgs(node, msgs);
-                    msgs = [];
-                }
-            }, interval);
+            this.on("close", function() {
+                clearInterval(timer);
+                node.msg_queue = [];
+            });
+        }
+        else if(mode === "concat") {
+            node.topics = (n.topics || []).map(function(x) {
+                return x.topic;
+            });
+            node.pending = {};
+            node.pending_count = 0;
+            this.on("input", function(msg) {
+                concat_msg(node, msg);
+            });
+            this.on("close", function(msg) {
+                node.pending = {};
+                node.pending_count = 0;
+            });
         }
         else {
             node.error("unexpected mode");
