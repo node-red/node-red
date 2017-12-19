f737ea96f309f1e5f59fc94a5f2687fae3d5d806
Dave Conway-Jones <conway@uk.ibm.com>
2017-12-19 17:52:35 +0000

    Don't send message if it doesn't exist. and give it time not to exist.
    to close #1527

--- nodes/core/core/89-trigger.js
+++ nodes/core/core/89-trigger.js
@@ -127,16 +127,18 @@ module.exports = function(RED) {
                     node.status({fill:"blue",shape:"dot",text:" "});
                 }
                 else if ((node.extend === "true" || node.extend === true) && (node.duration > 0)) {
-                    if (node.topics[topic].tout) { clearTimeout(node.topics[topic].tout); }
                     if (node.op2type === "payl") { node.topics[topic].m2 = RED.util.cloneMessage(msg.payload); }
+                    if (node.topics[topic].tout) { clearTimeout(node.topics[topic].tout); }
                     node.topics[topic].tout = setTimeout(function() {
                         if (node.op2type !== "nul") {
                             var msg2 = RED.util.cloneMessage(msg);
                             if (node.op2type === "flow" || node.op2type === "global") {
                                 node.topics[topic].m2 = RED.util.evaluateNodeProperty(node.op2,node.op2type,node,msg);
                             }
-                            msg2.payload = node.topics[topic].m2;
-                            node.send(msg2);
+                            if (node.topics[topic] !== undefined) {
+                                msg2.payload = node.topics[topic].m2;
+                                node.send(msg2);
+                            }
                         }
                         delete node.topics[topic];
                         node.status({});
