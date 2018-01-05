ff8773f6bd14583b2185f7dccf6586d7f7ed722e
Kazuki Nakanishi <kazuki.nakanishi.mg@hitachi.com>
2018-01-05 23:22:49 +0900

    Fix the problem that occurs by grunt coverage (#1537)

--- test/editor/wdio.conf.js
+++ test/editor/wdio.conf.js
@@ -26,7 +26,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/editor/**/*_spec.js'
+        './test/editor/**/*_uispec.js'
     ],
     // Patterns to exclude.
     exclude: [
