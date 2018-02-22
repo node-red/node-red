f21260370f161704ab053454f157590b7018f9da
Kazuki-Nakanishi <kazuki.nakanishi.vr@hitachi-solutions.com>
2018-02-22 14:10:31 +0900

    Fixed the problems when using a node without defaults

--- editor/js/history.js
+++ editor/js/history.js
@@ -191,7 +191,7 @@ RED.history = (function() {
             } else if (ev.t == "edit") {
                 for (i in ev.changes) {
                     if (ev.changes.hasOwnProperty(i)) {
-                        if (ev.node._def.defaults[i] && ev.node._def.defaults[i].type) {
+                        if (ev.node._def.defaults && ev.node._def.defaults[i] && ev.node._def.defaults[i].type) {
                             // This is a config node property
                             var currentConfigNode = RED.nodes.node(ev.node[i]);
                             if (currentConfigNode) {
