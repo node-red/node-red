626d012775898a2bcf592ffd05c8d57a2958c6c5
Nick O'Leary <nick.oleary@gmail.com>
2018-05-21 16:14:43 +0100

    Do not disable the export-clipboard menu option with empty selection

--- editor/js/main.js
+++ editor/js/main.js
@@ -410,8 +410,8 @@
             {id:"menu-item-import-clipboard",label:RED._("menu.label.clipboard"),onselect:"core:show-import-dialog"},
             {id:"menu-item-import-library",label:RED._("menu.label.library"),options:[]}
         ]});
-        menuOptions.push({id:"menu-item-export",label:RED._("menu.label.export"),disabled:true,options:[
-            {id:"menu-item-export-clipboard",label:RED._("menu.label.clipboard"),disabled:true,onselect:"core:show-export-dialog"},
+        menuOptions.push({id:"menu-item-export",label:RED._("menu.label.export"),options:[
+            {id:"menu-item-export-clipboard",label:RED._("menu.label.clipboard"),onselect:"core:show-export-dialog"},
             {id:"menu-item-export-library",label:RED._("menu.label.library"),disabled:true,onselect:"core:library-export"}
         ]});
         menuOptions.push(null);
