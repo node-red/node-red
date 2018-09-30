5391fc962a43f2cc4ad5c672859d4a7f443f644a
Nick O'Leary <nick.oleary@gmail.com>
2018-09-30 21:28:44 +0100

    Update ACE to 1.4.1-src-min-noconflict

--- packages/node_modules/@node-red/editor-client/src/vendor/ace/snippets/swift.js
+++ packages/node_modules/@node-red/editor-client/src/vendor/ace/snippets/swift.js
@@ -1,1 +1,9 @@
-ace.define("ace/snippets/swift",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="swift"})
+ace.define("ace/snippets/swift",["require","exports","module"],function(e,t,n){"use strict";t.snippetText="",t.scope="swift"});
+                (function() {
+                    ace.require(["ace/snippets/swift"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
