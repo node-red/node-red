c3bc3f37804a54bd11896f96860ef96198e2c324
sakazuki <sakazuki@gmail.com>
2018-12-03 20:30:29 +0900

    fix typo

--- editor/js/ui/userSettings.js
+++ editor/js/ui/userSettings.js
@@ -121,7 +121,7 @@ RED.userSettings = (function() {
             title: "menu.label.other",
             options: [
                 {setting:"view-show-tips",oldSettings:"menu-menu-item-show-tips",label:"menu.label.showTips",toggle:true,default:true,onchange:"core:toggle-show-tips"},
-                {setting:"view-flow-vertical",label:"menu.label.view.flowVirtical",toggle:true,default:true,onchange:RED.view.vertical}
+                {setting:"view-flow-vertical",label:"menu.label.view.flowVertical",toggle:true,default:true,onchange:RED.view.vertical}
             ]
         }
     ];
