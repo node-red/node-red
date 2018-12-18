715cc77e763acc2e8d4e67c76f1852037ed8c548
Yuma Matsuura <yuma.matsuura.uj@hitachi-solutions.com>
2018-12-18 09:29:46 +0900

    Update UI test for mac os

---
+++ test/editor/pageobjects/util/key_page.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var os = require("os");
+
+var shortCutKeyMap = {
+    "selectAll": ['Control', 'a', 'Control'],
+    "selectToEnd": ['Control', 'Shift', 'End', 'Shift', 'Control'],
+};
+
+var shortCutKeyMapForMac = {
+    "selectAll": ['Command', 'a', 'Command'],
+    "selectToEnd": ['Command', 'Shift', 'ArrowDown', 'Shift', 'Command'],
+};
+
+function getShortCutKey(type) {
+    if (os.type() === "Darwin") {
+        return shortCutKeyMapForMac[type];
+    } else {
+        return shortCutKeyMap[type];
+    }
+}
+
+function selectAll() {
+    var key = getShortCutKey('selectAll');
+    return key;
+}
+
+function selectToEnd() {
+    var key = getShortCutKey('selectToEnd');
+    return key;
+}
+
+module.exports = {
+    selectAll: selectAll,
+    selectToEnd: selectToEnd,
+};
