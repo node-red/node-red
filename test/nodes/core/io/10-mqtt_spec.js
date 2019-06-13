2cff4c1f79a15514b4de27418c3ab447263ccadb
Simon Bishop <simon@tenacious.digital>
2019-06-13 14:43:23 +0200

    enhancement: MQTT explicit protocol version 5 support

---
+++ test/nodes/core/io/10-mqtt_spec.js
@@ -0,0 +1,335 @@
+describe('MQTT Node', function() {
+
+    let should = require("should");
+    let mqttNode = require("nr-test-utils").require("@node-red/nodes/core/io/10-mqtt.js");
+    let helper = require("node-red-node-test-helper");
+
+    this.timeout(10000);
+
+    function mockBroker({
+        id,
+        type,
+        z,
+        name,
+        broker,
+        port,
+        clientid,
+        usetls,
+        protocolversion,
+        keepalive,
+        cleansession,
+        birthTopic,
+        birthQos,
+        birthPayload,
+        closeTopic,
+        closeQos,
+        closePayload,
+        willTopic,
+        willQos,
+        willPayload
+    }) {
+        return {
+            "id": id || "broker",
+            "type": "mqtt-broker",
+            "z": z || "",
+            "name": name || "local",
+            "broker": broker || "127.0.0.1",
+            "port": port || "11883",
+            "clientid": clientid || "",
+            "usetls": usetls || false,
+            "protocolversion": protocolversion == null ? null : protocolversion || "5",
+            "keepalive": keepalive || "60",
+            "cleansession": cleansession || true,
+            "birthTopic": birthTopic || "",
+            "birthQos": birthQos || "0",
+            "birthPayload": birthPayload || "",
+            "closeTopic": closeTopic || "",
+            "closeQos": closeQos || "0",
+            "closePayload": closePayload || "",
+            "willTopic": willTopic || "",
+            "willQos": willQos || "0",
+            "willPayload": willPayload || ""
+        }
+    }
+
+    it('loads up with the correct default mqtt version', async () => {
+
+        let flow = [{
+            id: "broker",
+            type: "mqtt-broker",
+            broker: "127.0.0.1"
+        }];
+        let mqttBroker = await loadNode(flow, "broker");
+        mqttBroker.should.have.property("protocolversion", 3);
+        helper.unload();
+    });
+
+    it('loads up with the correct mqtt version - specified', async () => {
+
+        let flow = [{
+            id: "broker",
+            type: "mqtt-broker",
+            broker: "127.0.0.1",
+            protocolversion: 4
+        }];
+        let mqttBroker = await loadNode(flow, "broker");
+        mqttBroker.should.have.property("protocolversion", 4);
+        helper.unload();
+    });
+
+    it('gets the correct parameters from the broker - protocol version 4', async () => {
+
+        let id = "broker";
+        let protocolversion = 4;
+        let broker = mockBroker({
+            protocolversion
+        });
+
+        let flow = [
+            broker,
+            {
+                id: "n1",
+                type: "mqtt in",
+                broker: "broker"
+            }
+        ];
+
+        let mqttNode = await loadNode(flow, "n1");
+        mqttNode.brokerConn.options.should.have.property("protocolVersion", 4);
+        helper.unload();
+    });
+
+    it('gets the correct parameters from the broker - protocol version 5', async () => {
+
+        let id = "broker";
+        let protocolversion = 5;
+        let broker = mockBroker({
+            protocolversion
+        });
+
+        let flow = [
+            broker,
+            {
+                id: "n1",
+                type: "mqtt in",
+                broker: "broker"
+            }
+        ];
+
+        let mqttNode = await loadNode(flow, "n1");
+        mqttNode.brokerConn.options.should.have.property("protocolVersion", 5);
+        helper.unload();
+    });
+
+    it('tests the broker node is able to connect to an mqtt broker', async () => {
+
+        let flow = [{
+            id: "broker",
+            type: "mqtt-broker",
+            broker: "127.0.0.1",
+            protocolversion: 3
+        }];
+        let mqttServer = await startMQTTServer();
+        let mqttBroker = await loadNode(flow, "broker");
+        mqttBroker.connected.should.equal(false);
+        await connectMqttNode(mqttBroker);
+        mqttBroker.client.end();
+        helper.unload();
+        await stopMQTTServer(mqttServer);
+    });
+
+    it('tests the broker node is able to subscribe and publish', async () => {
+
+        let flow = [{
+            id: "broker",
+            type: "mqtt-broker",
+            broker: "127.0.0.1",
+            protocolversion: 3
+        }];
+
+        let publishMessage = {
+            topic: 'test/topic',
+            qos: 2,
+            payload: {
+                test: "payload"
+            }
+        };
+
+        let mqttServer = await startMQTTServer();
+        let publishMessageCloned = JSON.parse(JSON.stringify(publishMessage));
+        let mqttBroker = await loadNode(flow, "broker");
+        await connectMqttNode(mqttBroker);
+        publishMessageCloned.should.eql(publishMessage);
+        let result = await subscribeAndPublishMqttBrokerNode(mqttBroker, publishMessage);
+        publishMessageCloned.should.eql(publishMessage); //ensure that the subscribe and publish has not modified our publish message
+        result.topic.should.eql(publishMessage.topic);
+        result.payload.should.eql(publishMessage.payload);
+        mqttBroker.client.end();
+        helper.unload();
+        await stopMQTTServer(mqttServer);
+    });
+
+    it.skip('tests the broker node is able to connect, but fail to subscribe', async () => {
+
+        let flow = [{
+            id: "broker",
+            type: "mqtt-broker",
+            broker: "127.0.0.1",
+            protocolversion: 3
+        }];
+
+        let publishMessage = {
+            topic: 'test/topic',
+            qos: 2,
+            payload: {
+                test: "payload"
+            }
+        };
+
+        let mqttServer = await startMQTTServer();
+        let mqttBroker = await loadNode(flow, "broker");
+        await connectMqttNode(mqttBroker);
+        await stopMQTTServer(mqttServer);//stop the MQTT server
+
+        try{
+            let result = await subscribeMqttBrokerNode(mqttBroker, publishMessage);
+            //this should not work if we are "connected" to a dead MQTT broker
+            throw new Error('unexpected success');
+        }catch(e){
+            e.message.should.not.equal('unexpected success');
+        }
+
+        mqttBroker.client.end();
+        helper.unload();
+    });
+
+    it.skip('tests the broker node is able to connect, but fail to publish', async () => {
+
+        let flow = [{
+            id: "broker",
+            type: "mqtt-broker",
+            broker: "127.0.0.1",
+            protocolversion: 3
+        }];
+
+        let publishMessage = {
+            topic: 'test/topic',
+            qos: 2,
+            payload: {
+                test: "payload"
+            }
+        };
+
+        let mqttServer = await startMQTTServer();
+        let mqttBroker = await loadNode(flow, "broker");
+        await connectMqttNode(mqttBroker);
+        await stopMQTTServer(mqttServer);//stop the MQTT server
+
+        try{
+            let result = await publishMqttBrokerNode(mqttBroker, publishMessage);
+            //this should not work if we are "connected" to a dead MQTT broker
+            throw new Error('unexpected success');
+        }catch(e){
+            e.message.should.not.equal('unexpected success');
+        }
+
+        mqttBroker.client.end();
+        helper.unload();
+    });
+
+    //helper functions
+
+    async function startMQTTServer(){
+        return new Promise((resolve, reject)=>{
+            let mosca = require('mosca');
+            let moscaSettings = {
+                port: 1883,
+                persistence: {
+                    // Needs for retaining messages.
+                    factory: mosca.persistence.Memory
+                }
+            };
+            let mqttServer = new mosca.Server(moscaSettings, function(e){
+                if (e) {return reject(e);}
+                resolve(mqttServer);
+            });
+        });
+    }
+
+    async function stopMQTTServer(mqttServer){
+        return new Promise((resolve, reject)=>{
+            mqttServer.close(function(e){
+                if (e) {
+                    return reject(e);
+                }
+                resolve();
+            });
+        });
+    }
+
+    async function connectMqttNode(mqttBroker) {
+        return new Promise(function(resolve, reject) {
+            mqttBroker.connect();
+            setTimeout(() => {
+                try {
+                    mqttBroker.connected.should.equal(true);
+                    resolve();
+                } catch (e) {
+                    reject(e);
+                }
+            }, 1000);
+        });
+    }
+
+    async function loadNode(flow, returnNode) {
+        return new Promise(function(resolve, reject) {
+            helper.load(mqttNode, flow, function(e) {
+                if (e) {
+                    return reject(e);
+                }
+                resolve(helper.getNode(returnNode));
+            });
+        })
+    }
+
+    async function subscribeAndPublishMqttBrokerNode(mqttBroker, publishMessage) {
+        return new Promise((resolve, reject) => {
+            let timedOut = setTimeout(() => {
+                reject(new Error('subscribeAndPublishMqttBrokerNode timed out'));
+            }, 1500);
+            mqttBroker.subscribe(publishMessage.topic, publishMessage.qos, (topic, payloadBuffer) => {
+                clearTimeout(timedOut);
+                let payload = JSON.parse(payloadBuffer.toString());
+                resolve({
+                    topic,
+                    payload
+                });
+            });
+            mqttBroker.publish(publishMessage);
+        });
+    }
+
+    async function subscribeMqttBrokerNode(mqttBroker, publishMessage) {
+        return new Promise((resolve, reject) => {
+            try{
+                mqttBroker.subscribe(publishMessage.topic, publishMessage.qos, (topic, payloadBuffer) => {
+
+                });
+                resolve();
+            }catch(e){
+                reject(e);
+            }
+        });
+    }
+
+    async function publishMqttBrokerNode(mqttBroker, publishMessage) {
+        return new Promise((resolve, reject) => {
+            try{
+                mqttBroker.publish(publishMessage);
+                resolve();
+            }catch(e){
+                reject(e);
+            }
+        });
+    }
+});
--- test/unit/@node-red/util/lib/i18n_spec.js
+++ test/unit/@node-red/util/lib/i18n_spec.js
@@ -13,14 +13,83 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
-
-
- var NR_TEST_UTILS = require("nr-test-utils");
-
- var i18n = NR_TEST_UTILS.require("@node-red/util").i18n;
-
+var NR_TEST_UTILS = require("nr-test-utils");
+var i18n = NR_TEST_UTILS.require("@node-red/util").i18n;
 
 describe("@node-red/util/i18n", function() {
-    it.skip('more tests needed', function(){})
+
+    const path = require('path');
+    const fs = require('fs-extra');
+
+    it('ensures fallback file has the mqtt.label.protocolversion key', function() {
+
+        var enUS = require(path.resolve(__dirname, '../../../../../packages/node_modules/@node-red/nodes/locales/en-US/messages.json'));
+        var keys = deepKeys(enUS, []);
+        true.should.equal(keys.indexOf('mqtt.label.protocolversion') > -1);
+    });
+
+    it.skip('ensures all messages.json files have the same keys', function() {
+        //fails, but because we do a fallback to en-us this is ok
+        //leaving this here in case we want to be able to use this to review translation coverage
+        let results = getFiles('../../../../../packages/node_modules/@node-red/nodes/locales')
+            .map((path) => {
+                //grab the paths
+                var paths = deepKeys(require(path), []);
+                return paths.join('');
+            })
+            .filter((paths, pos, arr) => {
+                //dedup
+                return arr.indexOf(paths) == pos;
+            });
+        //should only have one set of paths
+        results.length.should.equal(1);
+    });
+
+    it.skip('more tests needed', function() {});
+
+    //helper functions
+
+    function getFile(relativePath) {
+        let basePath = path.resolve(__dirname, relativePath);
+        return fs.readdirSync(basePath)
+            .map(function(path) {
+                return [basePath, path, 'messages.json'].join('/');
+            });
+    }
+
+    function getFiles(relativePath, regex) {
+        let basePath = path.resolve(__dirname, relativePath);
+        return fs.readdirSync(basePath)
+            .map(function(path) {
+                return [basePath, path, 'messages.json'].join('/');
+            });
+    }
+
+    //grabbed from deep-keys: https://github.com/a8m/deep-keys
+    //not certain of maintainer
+    function deepKeys(obj, stack, parent, intermediate) {
+        Object.keys(obj).forEach(function(el) {
+            // Escape . in the element name
+            var escaped = el.replace(/\./g, '\\\.');
+            // If it's a nested object
+            if (isObject(obj[el]) && !Array.isArray(obj[el])) {
+                // Concatenate the new parent if exist
+                var p = parent ? parent + '.' + escaped : parent;
+                // Push intermediate parent key if flag is true
+                if (intermediate) {
+                    stack.push(parent ? p : escaped);
+                }
+                deepKeys(obj[el], stack, p || escaped, intermediate);
+            } else {
+                // Create and save the key
+                var key = parent ? parent + '.' + escaped : escaped;
+                stack.push(key)
+            }
+        });
+        return stack;
+    }
+
+    function isObject(value) {
+        return value !== null && typeof value === 'object' && !(value instanceof Date);
+    }
 });
