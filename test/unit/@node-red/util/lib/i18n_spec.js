6e765c890d6785e58cd3ab6c3b3f0b715774786e
Simon Bishop <simonbishop@Simons-MBP-2.localdomain>
2019-06-13 12:22:06 +0200

    enhancement: MQTT explicit protocol version 5 support

--- test/unit/@node-red/util/lib/i18n_spec.js
+++ test/unit/@node-red/util/lib/i18n_spec.js
@@ -13,14 +13,84 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-
-
-
- var NR_TEST_UTILS = require("nr-test-utils");
-
- var i18n = NR_TEST_UTILS.require("@node-red/util").i18n;
-
+var NR_TEST_UTILS = require("nr-test-utils");
+var i18n = NR_TEST_UTILS.require("@node-red/util").i18n;
 
 describe("@node-red/util/i18n", function() {
-    it.skip('more tests needed', function(){})
+
+    const path = require('path');
+    const fs = require('fs-extra');
+
+    it('ensures all messages.json files have the mqtt.label.protocolversion key', function() {
+        let results = getFiles('../../../../../packages/node_modules/@node-red/nodes/locales')
+            .map((path) => {
+                //grab the paths
+                return deepKeys(require(path), []);
+            })
+            .filter((paths, pos, arr) => {
+                return paths.indexOf('mqtt.label.protocolversion') == -1;
+            });
+
+        //should have no results where this property key is missing
+        if (results.length > 0) {
+            throw new Error('some locale files are missing mqtt.label.protocolversion');
+        }
+    });
+
+    it.skip('ensures all messages.json files have the same keys', function() {
+        //fails, but because we do a fallback to en-us this is ok
+        //leaving this here in case we want to be able to use this to review translation coverage
+        let results = getFiles('../../../../../packages/node_modules/@node-red/nodes/locales')
+            .map((path) => {
+                //grab the paths
+                var paths = deepKeys(require(path), []);
+                return paths.join('');
+            })
+            .filter((paths, pos, arr) => {
+                //dedup
+                return arr.indexOf(paths) == pos;
+            });
+        //should only have one set of paths
+        results.length.should.equal(1);
+    });
+
+    it.skip('more tests needed', function() {});
+
+    //helper functions
+
+    function getFiles(relativePath, regex) {
+        let basePath = path.resolve(__dirname, relativePath);
+        return fs.readdirSync(basePath)
+            .map(function(path) {
+                return [basePath, path, 'messages.json'].join('/');
+            });
+    }
+
+    //grabbed from deep-keys: https://github.com/a8m/deep-keys
+    //not certain of maintainer
+    function deepKeys(obj, stack, parent, intermediate) {
+        Object.keys(obj).forEach(function(el) {
+            // Escape . in the element name
+            var escaped = el.replace(/\./g, '\\\.');
+            // If it's a nested object
+            if (isObject(obj[el]) && !Array.isArray(obj[el])) {
+                // Concatenate the new parent if exist
+                var p = parent ? parent + '.' + escaped : parent;
+                // Push intermediate parent key if flag is true
+                if (intermediate) {
+                    stack.push(parent ? p : escaped);
+                }
+                deepKeys(obj[el], stack, p || escaped, intermediate);
+            } else {
+                // Create and save the key
+                var key = parent ? parent + '.' + escaped : escaped;
+                stack.push(key)
+            }
+        });
+        return stack;
+    }
+
+    function isObject(value) {
+        return value !== null && typeof value === 'object' && !(value instanceof Date);
+    }
 });
