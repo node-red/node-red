6e3fa974ba361e1bf32620d5b7dcb109d1f86c85
Nick O'Leary <nick.oleary@gmail.com>
2019-08-06 16:32:46 +0100

    Remove all ui test dependencies from package.json
    Given chromedriver was already an extra dependency that needed to
    be manually installed, I have now moved all of the webdriver.io
    dependencies out as well.
    
    A new script has been added to install all of the ui test dependencies.
    
    The Grunt file has been updated on how it checks for the missing
    deps.

---
+++ scripts/install-ui-test-dependencies.sh
@@ -0,0 +1,7 @@
+npm install --no-save \
+ grunt-webdriver@^2.0.3 \
+ wdio-chromedriver-service@^0.1.5 \
+ wdio-mocha-framework@^0.6.4 \
+ wdio-spec-reporter@^0.1.5 \
+ webdriverio@^4.14.1 \
+ chromedriver@2
