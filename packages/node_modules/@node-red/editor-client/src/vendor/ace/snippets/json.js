aaf134b1c568c60c6af0c16761a7e9a3d8698577
Nick O'Leary <nick.oleary@gmail.com>
2020-09-16 19:13:40 +0100

    Update ACE to 1.4.12-src-min-noconflict Fixes #1988

--- packages/node_modules/@node-red/editor-client/src/vendor/ace/snippets/json.js
+++ packages/node_modules/@node-red/editor-client/src/vendor/ace/snippets/json.js
@@ -1,4 +1,4 @@
-ace.define("ace/snippets/json",["require","exports","module"],function(e,t,n){"use strict";t.snippetText=undefined,t.scope="json"});                (function() {
+;                (function() {
                     ace.require(["ace/snippets/json"], function(m) {
                         if (typeof module == "object" && typeof exports == "object" && module) {
                             module.exports = m;
