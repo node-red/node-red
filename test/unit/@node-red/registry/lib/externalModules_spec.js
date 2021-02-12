9c09ee3b7106e9df39ace68d61f6959daee7b04a
Nick O'Leary <nick.oleary@gmail.com>
2021-02-12 18:14:13 +0000

    Rework Function node module integration

---
+++ test/unit/@node-red/registry/lib/externalModules_spec.js
@@ -0,0 +1,20 @@
+    // init: init,
+    // register: register,
+    // checkFlowDependencies: checkFlowDependencies,
+    // require: requireModule
+    //
+
+const should = require("should");
+
+const fs = require("fs");
+const path = require("path");
+
+var NR_TEST_UTILS = require("nr-test-utils");
+
+var externalModules = NR_TEST_UTILS.require("@node-red/registry/lib/externalModules");
+
+
+describe("externalModules api", function() {
+    
+
+});
