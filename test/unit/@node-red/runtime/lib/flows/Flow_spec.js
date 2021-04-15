d2432716ea5054c83188342f5c336a50f5f8e9b7
Nick O'Leary <nick.oleary@gmail.com>
2021-04-15 15:30:02 +0100

    Fix hook requires in unit tests

--- test/unit/@node-red/runtime/lib/flows/Flow_spec.js
+++ test/unit/@node-red/runtime/lib/flows/Flow_spec.js
@@ -26,7 +26,7 @@ var flowUtils = NR_TEST_UTILS.require("@node-red/runtime/lib/flows/util");
 var Flow = NR_TEST_UTILS.require("@node-red/runtime/lib/flows/Flow");
 var flows = NR_TEST_UTILS.require("@node-red/runtime/lib/flows");
 var Node = NR_TEST_UTILS.require("@node-red/runtime/lib/nodes/Node");
-var hooks = NR_TEST_UTILS.require("@node-red/runtime/lib/hooks");
+var hooks = NR_TEST_UTILS.require("@node-red/util/lib/hooks");
 var typeRegistry = NR_TEST_UTILS.require("@node-red/registry");
 
 
