70e768f4d86067739208891ceac4e164d4d0e758
andrew.greene <andrew@leapfrog.ai>
2021-12-08 18:01:31 -0700

    Remove known unused files

--- test/unit/@node-red/registry/lib/resources/local/MultipleNodes1/MultipleNodes1.js
+++
@@ -1,7 +0,0 @@
-// A test node that exports a function
-module.exports = function(RED) {
-    function TestNode1(n) {}
-    RED.nodes.registerType("test-node-multiple-1a",TestNode1);
-    function TestNode2(n) {}
-    RED.nodes.registerType("test-node-multiple-1b",TestNode2);
-}
--- test/unit/@node-red/registry/lib/resources/local/TestNode3/TestNode3.js
+++
@@ -1,7 +0,0 @@
-// A test node that exports a function which returns a rejecting promise
-
-module.exports = function(RED) {
-    return new Promise(function(resolve,reject) {
-        reject("fail");
-    });
-}
