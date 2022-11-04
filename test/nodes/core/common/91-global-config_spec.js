f23d0480e4bb30c09e1881ce7545159ee8453fca
Hiroyasu Nishiyama <hiroyasu.nishiyama.uq@hitachi.com>
2022-11-04 18:42:51 +0900

    add global environment variable feature

---
+++ test/nodes/core/common/91-global-config_spec.js
@@ -0,0 +1,63 @@
+/**
+ * Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var should = require("should");
+var config = require("nr-test-utils").require("@node-red/nodes/core/common/91-global-config.js");
+var inject = require("nr-test-utils").require("@node-red/nodes/core/common/20-inject.js");
+var helper = require("node-red-node-test-helper");
+
+describe('unknown Node', function() {
+
+    afterEach(function() {
+        helper.unload();
+    });
+
+    it('should be loaded', function(done) {
+        var flow = [{id:"n1", type:"global-config", name: "XYZ" }];
+        helper.load(config, flow, function() {
+            var n1 = helper.getNode("n1");
+            n1.should.have.property("name", "XYZ");
+            done();
+        });
+    });
+
+    it('should access global environment variable', function(done) {
+        var flow = [{id:"n1", type:"global-config", name: "XYZ",
+                     env: [ {
+                         name: "X",
+                         type: "string",
+                         value: "foo"
+                     }]
+                    },
+                    {id: "n2", type: "inject", topic: "t1", payload: "X", payloadType: "env", wires: [["n3"]], z: "flow"},
+                    {id: "n3", type: "helper"}
+                   ];
+        helper.load([config, inject], flow, function() {
+            var n2 = helper.getNode("n2");
+            var n3 = helper.getNode("n3");
+            n3.on("input", (msg) => {
+                try {
+                    msg.should.have.property("payload", "foo");
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
+            n2.receive({});
+        });
+    });
+
+});
